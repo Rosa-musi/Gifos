e40551f8e321921ea5bde5780893fc693b4fb233
Rosa-musi <67343452+Rosa-musi@users.noreply.github.com>
2020-10-04 15:42:03 -0500

    Update FavLocSt.js

--- scripts/FavLocSt.js
+++ scripts/FavLocSt.js
@@ -1,14 +1,12 @@
 function addToFavorites(gifUrl){
-    
     let new_data = gifUrl.getAttribute("data-gif-id");
     gifUrl.setAttribute("src", "imagenes/iconos/icon-fav-active.svg")
 
      if(localStorage.getItem('data') == null){
         localStorage.setItem('data', '[]');
     }
-    let old_data = JSON.parse(localStorage.getItem('data'));
-   
-    old_data.push(new_data);
 
+    let old_data = JSON.parse(localStorage.getItem('data'));
+    old_data.push(new_data);
     localStorage.setItem('data', JSON.stringify(old_data));  
 }
